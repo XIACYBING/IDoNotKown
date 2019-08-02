506f0541a0e57ecb572d559eaf3edb4dc70d7b26
xia <359474377@qq.com>
2019-08-02 10:17:53 +0800

    解决问题，完成codeGenerator的操作

---
+++ myBatisPlussample/src/main/java/com/hk/xia/mybatisplus/generator/code/dao/EmployeeDao.java
@@ -0,0 +1,16 @@
+package com.hk.xia.mybatisplus.generator.code.dao;
+
+import com.hk.xia.mybatisplus.generator.code.pojo.Employee;
+
+/**
+* 通用 Mapper 代码生成器
+*
+* @author mapper-generator
+*/
+public interface EmployeeDao extends tk.mybatis.mapper.common.Mapper<Employee> {
+
+}
+
+
+
+
---
+++ myBatisPlussample/src/main/java/com/hk/xia/mybatisplus/generator/code/dao/HkEmployeeDao.java
@@ -0,0 +1,16 @@
+package com.hk.xia.mybatisplus.generator.code.dao;
+
+import com.hk.xia.mybatisplus.generator.code.pojo.HkEmployee;
+
+/**
+* 通用 Mapper 代码生成器
+*
+* @author mapper-generator
+*/
+public interface HkEmployeeDao extends tk.mybatis.mapper.common.Mapper<HkEmployee> {
+
+}
+
+
+
+
